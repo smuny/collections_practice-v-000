60a8b718f455461cca4e2f808004719f30f2c329
Sharon Mun <sharonymun@gmail.com>
2018-06-21 18:57:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(ascending)
+  ascending.sort
+end
 
+def sort_array_desc(descending)
+  descending.sort.reverse
+end
+
+def sort_array_char_count(character_length)
+  character_length.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array.insert(1,array.delete_at(2))
+end
+
+def reverse_array(reverse_order)
+  reverse_order.reverse
+end
+
+def kesha_maker(complex)
+  complex.each do |dollar_sign|
+    dollar_sign.length[2] = "$"
+  end
+  complex
+end
+
+def find_a(find)
+end
+
+def sum_array(add)
+end
+
+def add_s
+end
