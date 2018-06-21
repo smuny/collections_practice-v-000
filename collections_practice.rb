fd6502da9d660fcd7b5306b4ba39e72e688f5f99
Sharon Mun <sharonymun@gmail.com>
2018-06-21 20:33:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(ascending)
+  ascending.sort
+end
+
+def sort_array_desc(descending)
+  descending.sort.reverse
+end
+
+def sort_array_char_count(character_length)
+  character_length.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array.insert(1,array.delete_at(2))
+end
+
+def reverse_array(reverse_order)
+  reverse_order.reverse
+end
+
+def kesha_maker(complex)
+  complex.each do |dollar_sign|
+    dollar_sign[2] = "$"
+  end
+  complex
+end
+
+def find_a(find)
+ find.select { |words| words.start_with?('a') }
+end
+
+def sum_array(add)
+  add.inject {|sum, n| sum + n }
+end
+
+def add_s(hard)
+  hard.each_with_index.collect do |element, index|
+    if element != "feet" 
+      element + "s"
+    else
+     element
+    end
+end
+end
 
